2dfbfa672493a4a197c2370fbea241c4678e4122
johnny630 <bakolama@gmail.com>
2021-12-01 14:52:17 +0800

    Adding loading and data states

--- src/App.js
+++ src/App.js
@@ -5,21 +5,24 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function fetchMoviesHandler() {
+    setIsLoading(true);
     // https://swapi.dev/
-    const response = await fetch('https://swapi.dev/api/films/')
+    const response = await fetch('https://swapi.dev/api/films/');
     const data = await response.json();
-    const transformedMovies = data.results.map(movieData => {
+    const transformedMovies = data.results.map((movieData) => {
       return {
         id: movieData.episode_id,
         title: movieData.title,
         openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_data
+        releaseDate: movieData.release_data,
       };
     });
 
     setMovies(transformedMovies);
+    setIsLoading(false);
   }
 
   return (
@@ -28,7 +31,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
